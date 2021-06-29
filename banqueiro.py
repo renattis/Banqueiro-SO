3e67fe6ff7ef9529f24e38e16d93267b45ddd308
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2021-06-29 15:07:35 -0300

    Update banqueiro.py

--- banqueiro.py
+++ banqueiro.py
@@ -1,15 +1,31 @@
-class Banqueiro:
-	def __init__(self, saldo, lista):
-		self.saldo = saldo
-		self.saldo_total = saldo
-		self.lista = lista
-		self.lista_espera = []
+import os
+from functions import *
 
 
-class Pessoa:
-	def __init__(self, nome, codigo):
-		self.nome = nome
-		self.codigo = codigo
-		self.saldo = 0
-		self.devendo = False
-		self.ciclo = 0
+def main():
+    print(16 * '#' + '-Bankers algorithm-' + 16 * '#')
+    qtd_processos = int(input('Quantos processos estão em execução? '))
+
+    qtd_tipos_recursos = int(input('Quantos tipos de recursos cada processo vai necessitar? '))
+
+    recursos_totais = get_recursos_totais(qtd_tipos_recursos)
+
+    matriz_alocados = get_matriz_alocados(qtd_processos, qtd_tipos_recursos)
+
+    recursos_alocados = get_vetor_recursos_alocados(matriz_alocados)
+
+    recursos_disponiveis = get_vetor_recursos_disponiveis(recursos_totais, recursos_alocados)
+
+    matriz_recursos_necessarios = get_matriz_recursos_necessarios(qtd_processos, qtd_tipos_recursos)
+
+    input('Pressione Enter para continuar.....')
+
+    os.system('clear')
+
+    imprimir_os_dados(recursos_totais, matriz_alocados, recursos_alocados, recursos_disponiveis, matriz_recursos_necessarios)
+
+    algoritmo_banqueiro(qtd_processos, qtd_tipos_recursos, recursos_disponiveis, matriz_recursos_necessarios, matriz_alocados)
+
+
+if __name__ == '__main__':
+    main()
